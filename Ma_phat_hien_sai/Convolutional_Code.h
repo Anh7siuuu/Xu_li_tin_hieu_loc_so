1d3cbb35acc94f34e65d75f05ab7f4795aa47602
Anh7siuuu <qcuong1968@gmail.com>
2025-05-14 23:46:37 +0700

    cap Nhat

--- Ma_phat_hien_sai/Convolutional_Code.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-// Hàm tạo mã chập (Convolutional Code)
-vector<vector<int>> convolutionalEncode(vector<int> &Data);
-// Hiển thị mã chập
-void Display_ConvolutionalCode(vector<vector<int>> &encodedData);
-
